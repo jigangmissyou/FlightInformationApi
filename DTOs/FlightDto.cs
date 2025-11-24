2780e3c6771b79bc3bfc6e7330cc27ced9e18932
Jigang Guo <jigang.guo@gmail.com>
2025-11-24 22:07:15 +1300

    Add flightDto

---
+++ DTOs/FlightDto.cs
@@ -0,0 +1,15 @@
+using FlightInformationApi.Models;
+
+namespace FlightInformationApi.DTOs;
+
+public class FlightDto
+{
+    public int Id { get; set; }
+    public string FlightNumber { get; set; } = string.Empty;
+    public string Airline { get; set; } = string.Empty;
+    public string DepartureAirport { get; set; } = string.Empty;
+    public string ArrivalAirport { get; set; } = string.Empty;
+    public DateTime DepartureTime { get; set; }
+    public DateTime ArrivalTime { get; set; }
+    public FlightStatus Status { get; set; }
+}
